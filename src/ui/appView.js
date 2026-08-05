5b5ba45024fc3f1d570c2659e9511a598d0bb7dc
Pedro Marques <52702280+pedrodinism@users.noreply.github.com>
2026-08-05 17:16:30 +0200

    added a modal to create new projects

--- src/ui/appView.js
+++ src/ui/appView.js
@@ -4,6 +4,7 @@ export function renderProjects() {
     const projectList = document.querySelector('#projectList')
     projectList.innerHTML = ''
     const projects = getProjects()
+    console.log(projects)
     projects.forEach((project) => {
             let div = document.createElement('div')
             div.classList.add('projectFolder')
@@ -21,14 +22,21 @@ export function renderLayout() {
     body.appendChild(mainContent)
     mainContent.appendChild(renderProjectContainer())
     mainContent.appendChild(renderToDosContainer())
+    renderProjectsDialog()
 }
 
 export function init() {
     document.addEventListener('click', (event) => {
+        event.preventDefault()
         switch(event.target.dataset.action) {
-            case 'add-project': 
-                createProject('Test')
+            case 'save-project':
+                closeDialog()
+                const projectTitle = document.querySelector('#inputProjectTitle').value
+                createProject(projectTitle)
                 renderProjects()
+                
+            case 'show-project-modal':
+                openDialog()
         }
 
     })
@@ -50,7 +58,7 @@ function renderProjectContainer() {
     
     const btnAddProject = document.createElement('button')
     btnAddProject.textContent = 'New project'
-    btnAddProject.setAttribute('data-action', 'add-project')
+    btnAddProject.setAttribute('data-action', 'show-project-modal')
     projectActions.appendChild(btnAddProject)
     projectContainer.appendChild(projectActions)
 
@@ -71,4 +79,37 @@ function renderMainContent() {
     const mainContent = document.createElement('div')
     mainContent.classList.add('mainContent')
     return mainContent
+}
+
+function renderProjectsDialog() {
+    const body = document.querySelector('body')
+    const projectDialog = document.createElement('dialog')
+    projectDialog.id = 'projectsDialog'
+
+    const titleDialog = document.createElement('div')
+    titleDialog.textContent = 'Edit project'
+    projectDialog.appendChild(titleDialog)
+
+    const projectForm = document.createElement('form')
+    const titleInput = document.createElement('input')
+    titleInput.placeholder = 'Title'
+    titleInput.type = 'text'
+    titleInput.id = 'inputProjectTitle'
+    projectForm.appendChild(titleInput)
+    titleDialog.appendChild(projectForm)
+
+    const saveProjectBtn = document.createElement('button')
+    saveProjectBtn.textContent = 'Save'
+    saveProjectBtn.setAttribute('data-action', 'save-project')
+    projectForm.appendChild(saveProjectBtn)
+
+    body.appendChild(projectDialog)
+}
+
+function closeDialog (id) {
+    document.querySelector('#projectsDialog').close()
+}
+
+function openDialog (id) {
+    document.querySelector('#projectsDialog').showModal()
 }
